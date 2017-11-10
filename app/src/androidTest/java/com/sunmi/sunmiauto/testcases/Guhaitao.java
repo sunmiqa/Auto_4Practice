d2cca8aa9b9a4d46367c93489afcbf82e64bd4d6
Guhaitao <641729039@qq.com>
2017-11-10 14:56:58 +0800

    第三次提交

--- app/src/androidTest/java/com/sunmi/sunmiauto/testcases/Guhaitao.java
+++ app/src/androidTest/java/com/sunmi/sunmiauto/testcases/Guhaitao.java
@@ -3,7 +3,9 @@ package com.sunmi.sunmiauto.testcases;
 import android.graphics.Point;
 import android.os.RemoteException;
 import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject2;
+import android.util.Log;
 
 import com.sunmi.sunmiauto.testutils.CommonAction;
 import com.sunmi.sunmiauto.testutils.TestUtils;
@@ -12,6 +14,12 @@ import com.sunmi.sunmiauto.testutils.UiobjectFinder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.sunmi.sunmiauto.testutils.TestUtils.checkScreenStatus;
 import static com.sunmi.sunmiauto.testutils.TestUtils.device;
 import static com.sunmi.sunmiauto.testutils.TestUtils.sleep;
 
@@ -33,10 +41,9 @@ public class Guhaitao {
         CommonAction.clickByText("下一步");
     }
 
-
     @Test
     public void test001apktegother() throws RemoteException {
-        TestUtils.checkScreenStatus();   //亮屏，保证可用
+        checkScreenStatus();   //亮屏，保证可用
         TestUtils.clearAllRecentApps();  //杀掉已启动应用
         int k = UiobjectFinder.findByText("设置").getVisibleBounds().centerX(); //定位“设置”横坐标
         int g = UiobjectFinder.findByText("设置").getVisibleBounds().centerY(); //定位“设置”纵坐标
@@ -55,6 +62,7 @@ public class Guhaitao {
         sleep(1000);
         UiobjectFinder.findByText("设置").drag(kgs,400);  //将“设置”拖动到指定坐标点
 
+
     }
 
 
@@ -68,4 +76,71 @@ public class Guhaitao {
         UiObject2 uiObject2 = UiobjectFinder.findByText("Opera Mini");  //返回对象
         Assert.assertNull("应用未删除",uiObject2);  //判断是否删除
     }
+    @Test
+    public void test004settingandapptore() throws  RemoteException {
+        checkScreenStatus();
+        TestUtils.clearAllRecentApps();
+        UiObject2 pages = UiobjectFinder.findById("com.woyou.launcher:id/page_indicator");
+
+        System.out.println("1111"+pages.getContentDescription());
+
+        String[] strings = pages.getContentDescription().split("：");
+        String[] strings1 = strings[1].split("，");
+
+        for(String s:strings1){
+            System.out.println(s);
+            getNum(s);
+        }
+
+    }
+
+    private void getNum(String string){
+        String regEx="[^0-9]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(string);
+       System.out.println( m.replaceAll("").trim());
+    }
+    @Test
+    public void test005settingandapptore() throws  RemoteException {
+        TestUtils.checkScreenStatus();
+        TestUtils.clearAllRecentApps();
+        UiObject2 page1 = UiobjectFinder.findById("com.woyou.launcher:id/page_indicator");
+        List<Integer> numbers = new ArrayList<>();
+        String s = "你好1，这是一个345测试提取24数字的测试5，希望645能够6成功10";
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(s.charAt(i));
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (Character.isDigit(s.charAt(j))) {
+                        ++i;
+                        sb.append(s.charAt(j));
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println(sb.toString());
+                numbers.add(Integer.parseInt(sb.toString()));
+            }
+        }
+
+        for(Integer i:numbers){
+            Log.d("TAG", "test005settingandapptore: ------------->"+i);
+        }
+    }
+
+    @Test
+    public void test006checkicon() throws RemoteException {
+        TestUtils.checkScreenStatus();
+        TestUtils.clearAllRecentApps();
+        sleep(2000);
+        CommonAction.swipeToRight();
+        int screenh = UiobjectFinder.findById("com.woyou.launcher:id/workspace").getVisibleBounds().height() -
+                UiobjectFinder.findById("com.woyou.launcher:id/search_drop_target_bar").getVisibleBounds().height() -
+                UiobjectFinder.findById("com.woyou.launcher:id/page_indicator").getVisibleBounds().height() -
+                UiobjectFinder.findById("com.woyou.launcher:id/hotseat").getVisibleBounds().height();   //定位“图标框”纵坐标
+        int screenw = UiobjectFinder.findById("com.woyou.launcher:id/workspace").getVisibleBounds().width();   //定位“图标框”横坐标
+        device.findObject();
+    }
 }
+
